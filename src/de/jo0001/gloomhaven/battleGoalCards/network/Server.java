2ac3313b2f947f060c8fc269844c26c5704148ee
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2020-11-19 16:54:15 +0100

    Optimized server stuff

--- src/de/jo0001/gloomhaven/battleGoalCards/network/Server.java
+++ src/de/jo0001/gloomhaven/battleGoalCards/network/Server.java
@@ -9,9 +9,9 @@ import java.util.Random;
 
 public class Server extends Thread {
     private ServerSocket serverSocket;
-    public static boolean firstClient = true;
-    private static int[] ids = {458, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474, 475, 476, 477, 478, 479, 480, 481};
-    private static List<Integer> usedIds = new ArrayList<>();
+    private static final int[] ids = {458, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474, 475, 476, 477, 478, 479, 480, 481};
+    private static final List<Integer> usedIds = new ArrayList<>();
+    private static final List<Socket> clients = new ArrayList<>();
 
     public Server(int port) {
         try {
@@ -26,18 +26,14 @@ public class Server extends Thread {
     }
 
     private void runServer() {
-        InputListenerReload inputListenerReload = new InputListenerReload();
+        Socket client;
         while (true) {
             try {
-                System.out.println("Waiting for client on port " + serverSocket.getLocalPort());
-                Socket client = serverSocket.accept();
-                if (firstClient) {
-                    System.out.println("Starting Listener for first client");
-                    inputListenerReload.start();
-                    firstClient = false;
-                }
-                inputListenerReload.addClient(client);
-                System.out.println("Client connected: " + client.getInetAddress());
+                System.out.println("Waiting for clients on port " + serverSocket.getLocalPort());
+                client = serverSocket.accept();
+                clients.add(client);
+                ServerThread serverThread = new ServerThread(client);
+                serverThread.start();
             } catch (IOException e) {
                 e.printStackTrace();
                 break;
@@ -45,8 +41,7 @@ public class Server extends Thread {
         }
     }
 
-    public static int getNumber() {
-        Random rand = new Random();
+    public static int getNumber(Random rand) {
         int id;
         do {
             id = ids[rand.nextInt(ids.length)];
@@ -55,10 +50,61 @@ public class Server extends Thread {
         return id;
     }
 
+    public static void notifyClients() {
+        System.out.println("Sending ids to clients...");
+        Random rand = new Random();
+        List<Socket> deadClients = new ArrayList<>();
+        for (Socket tmp : clients) {
+            try {
+                DataOutputStream out = new DataOutputStream(tmp.getOutputStream());
+                out.writeUTF(Server.getNumber(rand) + "," + Server.getNumber(rand));
+                out.flush();
+            } catch (IOException e) {
+                deadClients.add(tmp);
+            }
+        }
+        usedIds.clear();
+        clients.removeAll(deadClients);
+    }
+
     //Just for Testing
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Server s = new Server(58889);
         s.runServer();
     }
 
+}
+
+class ServerThread extends Thread {
+    private final DataInputStream inputStream;
+    private final Socket s;
+
+    public ServerThread(Socket s) throws IOException {
+        this.s = s;
+        inputStream = new DataInputStream(s.getInputStream());
+        System.out.println("Starting new ServerThread " + this.getName());
+    }
+
+    public void run() {
+        try {
+            String in;
+            while (true) {
+                in = inputStream.readUTF();
+                System.out.println("Found something from" + s.getLocalAddress());
+                if (in.equalsIgnoreCase("getnumber")) {
+                    System.out.println("Found 'getnumber'");
+                    Server.notifyClients();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("IO error in server thread");
+            try {
+                if (s != null) {
+                    s.close();
+                    System.out.println(" Socket Closed");
+                }
+            } catch (IOException ignored) {
+            }
+        }
+    }
 }
